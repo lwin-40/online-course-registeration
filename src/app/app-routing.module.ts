a58559efcceeee64b3284298c765e482fa158abf
Lwin Nandar Oo <69138640+lwin-40@users.noreply.github.com>
2020-10-12 21:29:28 +0630

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {RecordComponent} from "./record/record.component";
+import {ShowComponent} from "./show/show.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path : '' , redirectTo : '/login' , pathMatch : 'full'},
+  { path : 'login' , component : LoginComponent },
+  { path : 'register' , component : RegisterComponent },
+  { path : 'record' , component : RecordComponent },
+  {path : 'show' , component : ShowComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { LoginComponent ; RegisterComponent ; RecordComponent ; ShowComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecordComponent } from './record/record.component';
+import { ShowComponent } from './show/show.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecordComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
